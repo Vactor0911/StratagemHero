5159580346f5f95b6452550e38ebf86871635c2b
Vactor0911 <vactor0911@gmail.com>
2024-03-14 17:51:11 +0900

    Added background image

---
+++ src/Classes.java
@@ -0,0 +1,118 @@
+import java.awt.*;
+import java.awt.image.*;
+import java.io.Serializable;
+import javax.swing.*;
+
+
+class ColorizeFilter extends RGBImageFilter implements Serializable {
+	private static final long serialVersionUID = 1L;
+    private int color;
+
+    public ColorizeFilter(int color) {
+        this.color = color;
+        canFilterIndexColorModel = true;
+    }
+    
+    public static Image colorizeImg(Image img, Color color) {
+		RGBImageFilter filter = new ColorizeFilter( color.getRGB() );
+		ImageProducer producer = new FilteredImageSource(img.getSource(), filter);
+		Image coloredImg = Toolkit.getDefaultToolkit().createImage(producer);
+		return coloredImg;
+	}
+
+    @Override
+    public int filterRGB(int x, int y, int rgb) {
+        int alpha = (rgb >> 24) & 0xFF;
+        int red = (color >> 16) & 0xFF;
+        int green = (color >> 8) & 0xFF;
+        int blue = color & 0xFF;
+        return (alpha << 24) | (red << 16) | (green << 8) | blue;
+    }
+} //ColorizeFilter Class
+
+
+class MyPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private final Image imgSuperEarth = new ImageIcon("rsc/SuperEarthEmblem.png").getImage();
+	private final Image coloredImage;
+	private final int WIDTH;
+	private final int HEIGHT;
+	private final Color COLOR_SUPER_EARTH = Color.GRAY;
+	private final Color COLOR_BACKGROUND = Color.DARK_GRAY;
+	private final Color COLOR_LINE = Color.WHITE;
+	
+	public MyPanel() {
+		WIDTH = imgSuperEarth.getWidth(null);
+		HEIGHT = imgSuperEarth.getHeight(null);
+		coloredImage = ColorizeFilter.colorizeImg(imgSuperEarth, COLOR_SUPER_EARTH);
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		//Background Coloring
+		g.setColor(COLOR_BACKGROUND);
+		g.fillRect( 0, 0, getWidth(), getHeight() );
+		
+		//Draw SuperEarth Emblem
+		double scaleX = (double)this.getWidth() / (double)WIDTH;
+		double scaleY = (double)this.getHeight() / (double)HEIGHT;
+		double scale = Math.min(scaleX, scaleY) * 0.5;
+		
+		int scaledWidth = (int)Math.ceil(WIDTH * scale);
+		int scaledHeight = (int)Math.ceil(HEIGHT * scale);
+		int x = (int)(this.getWidth() * 0.5 - scaledWidth * 0.5);
+		int y = (int)(this.getHeight() * 0.5 - scaledHeight * 0.5);
+		
+		
+		g.drawImage(coloredImage, x, y, scaledWidth, scaledHeight, this);
+		
+		//Draw Lines
+		g.setColor(COLOR_LINE);
+		int lineHeight = (int)(scaledHeight * 0.02);
+		int lineY = (int)(scaledHeight * 0.25);
+		g.fillRect(0, lineY - lineHeight / 2, getWidth(), lineHeight);
+		g.fillRect(0, getHeight() - lineY - lineHeight / 2, getWidth(), lineHeight);
+	} //paintComponent()
+} //MyPanel Class
+
+
+class ImagePanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private Image image;
+	private Color bgColor;
+	int width, height;
+	
+	public ImagePanel(Image image, Color bgColor) {
+		this.image = image;
+		this.bgColor = bgColor;
+		width = image.getWidth(null);
+		height = image.getHeight(null);
+	}
+	
+	public ImagePanel(Image image) {
+		this(image, null);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.setColor(bgColor);
+		g.fillRect( 0, 0, getWidth(), getHeight() );
+		
+		double scaleX = (double)this.getWidth() / (double)width;
+		double scaleY = (double)this.getHeight() / (double)height;
+		double scale = Math.min(scaleX, scaleY);
+		
+		int scaledWidth = (int)Math.ceil(width * scale);
+		int scaledHeight = (int)Math.ceil(height * scale);
+		int x = (int)(this.getWidth() * 0.5 - scaledWidth * 0.5);
+		int y = (int)(this.getHeight() * 0.5 - scaledHeight * 0.5);
+		
+		g.drawImage(image, x, y, scaledWidth, scaledHeight, this);
+		
+		g.setColor(Color.red);
+		g.fillRect(x, y, scaledWidth, scaledHeight);
+	} //paintComponent()
+} //ImagePanel Class
--- src/Frame.java
+++ src/Frame.java
@@ -1,58 +1,37 @@
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class Frame extends JFrame implements KeyListener {
 	private static final long serialVersionUID = 1L;
 	
-	//Images
-	
 	//Components
-	private BackgroundPanel backgroundPanel = new BackgroundPanel();
+	private JPanel panels = new JPanel( new CardLayout() );
+	private MyPanel pnlMain = new MyPanel();
+	private MyPanel pnlGame = new MyPanel();
+
+	//Settings
+	private static final String FONT_NAME = "맑은 고딕";
 	
-	private String strMenu = "Start";
+	private String strMenu = "Main";
 
 	public Frame() {
 		setTitle("Stratagem Hero");
 		setSize(1000, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		setLocationRelativeTo(null); //Move Frame to Middle
 		
 		//Initialize
 		setMinimumSize( new Dimension(1000, 500) );
 		addKeyListener(this);
 		
-		//Move Frame to Middle
-		Dimension resolution = Toolkit.getDefaultToolkit().getScreenSize();
-		int x = (int)(resolution.getWidth() * 0.5) - (int)(getWidth() * 0.5);
-		int y = (int)(resolution.getHeight() * 0.5) - (int)(getHeight() * 0.5);
-		setLocation(x, y);
+		//Draw Screen
+		//	Main Screen
+		panels.add(pnlMain, "Main");
 		
-		//Initial Screen
-		add(backgroundPanel, BorderLayout.CENTER);
+		add(panels);
 	}
-	
-	//setMenu
-	public void setMenu(String menu) {
-		if (menu == strMenu) {
-			return;
-		}
-		strMenu = menu;
-		getContentPane().removeAll();
-		add(backgroundPanel, BorderLayout.CENTER);
-		System.out.println("Menu >> " + menu);
-		
-		switch (menu) {
-			case "Main": //Main menu
-				break;
-			default: //Invalid menu name
-				return;
-		}
-		
-		repaint();
-	} //setMenu
 		
 	//KeyListener
 	@Override
@@ -61,68 +40,12 @@ public class Frame extends JFrame implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		switch (strMenu) {
-			case "Start":
-				setMenu("Main");
+			case "Main":
 				break;
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) { }
-}
-
-//ImagePanel Class
-class ImagePanel extends JPanel {
-	private static final long serialVersionUID = 1L;
-	private Image image;
-	private Color bgColor;
-	int width, height;
 	
-	public ImagePanel(Image image, Color bgColor) {
-		this.image = image;
-		this.bgColor = bgColor;
-		width = image.getWidth(null);
-		height = image.getHeight(null);
-	}
-	
-	public ImagePanel(Image image) {
-		this(image, null);
-	}
-	
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		if (bgColor != null) {
-			g.setColor(bgColor);
-			g.fillRect( 0, 0, getWidth(), getHeight() );
-		}
-		
-		double scaleX = (double)this.getWidth() / (double)width;
-		double scaleY = (double)this.getHeight() / (double)height;
-		double scale = Math.min(scaleX, scaleY);
-		
-		int scaledWidth = (int)Math.ceil(width * scale);
-		int scaledHeight = (int)Math.ceil(height * scale);
-		int x = (int)(this.getWidth() * 0.5 - scaledWidth * 0.5);
-		int y = (int)(this.getHeight() * 0.5 - scaledHeight * 0.5);
-		
-		g.drawImage(image, x, y, scaledWidth, scaledHeight, this);
-	}
-} //ImagePanel Class
-
-class BackgroundPanel extends ImagePanel {
-	private static final long serialVersionUID = 1L;
-	private static final Image imgSuperEarth = new ImageIcon("rsc/SuperEarthLogo.png").getImage();
-
-	public BackgroundPanel() {
-		super(imgSuperEarth, Color.GRAY);
-	}
-	
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		g.setColor(Color.YELLOW);
-		g.fillRect(0, 50, getWidth(), 5);
-	}
-}
+} //Frame Class
